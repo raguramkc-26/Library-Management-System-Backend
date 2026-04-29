5d300c65b8ee418f391a2b8951a4f7da40a862a1
raguramkc-26 <raguramkc038@gmail.com>
2026-04-29 17:45:53 +0530

    changes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../middlewares/upload");
 
 const {
   getAdminStats,
@@ -8,8 +7,7 @@ const {
   notifyAllUsers,
   getRecentActivity,
   getTopBooks,
-  addBook,
-  getAllUsers, 
+  getAllUsers,
   updateUserRole,
   deleteUser
 } = require("../controllers/adminController");
@@ -20,29 +18,16 @@ const { isAuthenticated, allowRoles } = require("../middlewares/auth");
 router.get("/stats", isAuthenticated, allowRoles("admin"), getAdminStats);
 router.get("/stats/monthly", isAuthenticated, allowRoles("admin"), getMonthlyStats);
 
-// USERS 
+// USERS
 router.get("/users", isAuthenticated, allowRoles("admin"), getAllUsers);
+router.put("/users/:id", isAuthenticated, allowRoles("admin"), updateUserRole);
+router.delete("/users/:id", isAuthenticated, allowRoles("admin"), deleteUser);
 
 // NOTIFICATIONS
 router.post("/notify-all", isAuthenticated, allowRoles("admin"), notifyAllUsers);
 
-// DASHBOARD DATA
+// DASHBOARD
 router.get("/recent", isAuthenticated, allowRoles("admin"), getRecentActivity);
 router.get("/top-books", isAuthenticated, allowRoles("admin"), getTopBooks);
-
-// BOOK CREATION
-router.post(
-  "/books",
-  isAuthenticated,
-  allowRoles("admin"),
-  upload.single("image"),
-  addBook
-);
-
-//updateUser
-router.put("/users/:id", isAuthenticated, allowRoles("admin"), updateUserRole);
-
-//deleteUser
-router.delete("users/:id", isAuthenticated, allowRoles("admin"), deleteUser);
 
 module.exports = router;
