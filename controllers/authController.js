32589ff122b8389651a649a45f28aa2994c439aa
raguramkc-26 <raguramkc038@gmail.com>
2026-05-01 17:51:37 +0530

    done

--- controllers/authController.js
+++ controllers/authController.js
@@ -231,4 +231,37 @@ const authController = {
   },
 };
 
+//update profile
+updateProfile: async (req, res) => {
+  try {
+    const { name, email } = req.body;
+
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    user.name = name || user.name;
+    user.email = email || user.email;
+
+    await user.save();
+
+    res.json({
+      success: true,
+      message: "Profile updated",
+      user,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Update failed",
+    });
+  }
+},
+
 module.exports = authController;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,4 +15,7 @@ authRouter.post("/logout", isAuthenticated, logout);
 authRouter.post("/forgot-password", authController.forgotPassword);
 authRouter.post("/reset-password/:token", authController.resetPassword);
 
+//update Profile
+authRouter.put("/update-profile",isAuthenticated,authController.updateProfile);
+
 module.exports = authRouter; 
