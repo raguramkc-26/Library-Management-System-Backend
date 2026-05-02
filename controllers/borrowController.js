d1f96da6001bf95ee31005a7c39c094cb7a8c015
raguramkc-26 <raguramkc038@gmail.com>
2026-05-02 18:45:05 +0530

    done

--- controllers/borrowController.js
+++ controllers/borrowController.js
@@ -21,7 +21,7 @@ const borrowBook = async (req, res) => {
       return res.status(404).json({ message: "Book not found" });
     }
 
-    if (book.status !== "available") {
+    if (book.status.toLowerCase() !== "available") {
       return res.status(400).json({ message: "Book not available" });
     }
 
